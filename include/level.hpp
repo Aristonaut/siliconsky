7b8796c396e8729d7ddb7fd00489cd1ede13091a
Cory Beutler <cg.beutler@gmail.com>
2016-03-17 23:58:53 -0600

    added a level file

---
+++ include/level.hpp
@@ -0,0 +1,19 @@
+#ifndef SILICON_SKY_HPP
+#define SILICON_SKY_HPP
+
+//> includes
+
+//> create namespace?
+
+/**
+ * A level object loaded from a file or json object
+ * Levels consist of a collection of environment layers, a collection of
+ * lighting layers, a collection of static asset layers, a collection of actor
+ * layers, and a collection of event triggers.
+ **/
+class Level
+{
+
+};
+
+#endif // SILICON_SKY_HPP
